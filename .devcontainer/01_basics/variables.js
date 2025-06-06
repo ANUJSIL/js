327aa7a3ade7ff4a6aa26057c1d1b1e42e3fffb3
ANUJSIL <anujsilsilanuj9696@gmail.com>
2025-06-06 08:20:51 +0000

    config done

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,18 @@
+const accountId ="12456666"
+let accountEmail ="anuj@gmail.com"
+var accountPassword="23388575"
+accountCity="KOLKATa"
+let accountState;
+
+//accountId=2//not allowed
+
+
+accountEmail ="df@gmail.com"
+accountPassword="21212121"
+accountCity="BENGALURU"
+console.log(accountId);
+
+/* prefer not to use var
+beacause of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState])
