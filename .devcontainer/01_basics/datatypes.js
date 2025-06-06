88de1785d06a009a8f0da03b459e853d340009ed
ANUJSIL <anujsilsilanuj9696@gmail.com>
2025-06-06 09:03:49 +0000

    datatypes

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,19 @@
+let name = "ANUJ"
+let age = 18
+let isLoggedIn = false
+
+
+//DATATYPES---
+//number => 2 power 53
+//bigint
+//string => ""
+//boolean =>true/false
+//null =>standalone value
+//undefined =>
+//symbol
+
+//object
+
+
+console.log(typeof"anuj"); // this is string datatype
+console.log(typeof  null); //this is object
