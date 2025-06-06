be1a706f6e11ebd22d2ed183eb527150cad47b98
ANUJSIL <anujsilsilanuj9696@gmail.com>
2025-06-06 09:04:23 +0000

    conversion operation

---
+++ .devcontainer/01_basics/conversionoperation.js
@@ -0,0 +1,28 @@
+let score ="123"// we check null , undefined ,true, false
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NAN
+//true =>1; false => 0
+
+let isLoggedIn = 1
+
+let bolleanIsLOggedIn =Boolean(isLoggedIn)
+//console.log(booleanIsLogeedIn);
+
+//1=>true : 0=>false
+//""=>false
+//"hitesh"=>true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
